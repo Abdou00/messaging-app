6c2bf4a192f849299fe116888a5dc93fdadc9516
Abdou00 <abdoulaye_diarra@outlook.fr>
2024-01-09 11:41:10 +0100

    Ajout du dotenv

--- src/components/axios.js
+++ src/components/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import 'dotenv/config'
 
 const instance = axios.create({
-    baseURL: process.env.PORT || "http://localhost:9000"
+    baseURL: process.env.PORT || "http://localhost:10000"
 })
 
 export default instance
